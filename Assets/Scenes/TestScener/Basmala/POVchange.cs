dceb278c5a67452d0cb884ebc932329919c50340
byggemandb <basmalayj@hotmail.com>
2023-11-07 12:54:01 +0100

    pov edit

--- Assets/Scenes/TestScener/Basmala/POVchange.cs
+++ Assets/Scenes/TestScener/Basmala/POVchange.cs
@@ -4,41 +4,18 @@ using UnityEngine;
 
 public class POVchange : MonoBehaviour
 {
-
     public GameObject MainCamera;
     public GameObject Thirdcamera;
     void Start()
     {
         
     }
-
-    /*public void ChangePOV()
-    {
-        if
-        {
-            MainCamera.enabled = false;
-            Thirdcamera.enabled = true;
-        }
-        else
-        {
-            MainCamera.enabled = true;
-            Thirdcamera.enabled = false;
-        }
-    }
-    */
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
             Debug.Log("adhgadkhfg");
             MainCamera.SetActive(!MainCamera.activeSelf);
-            Thirdcamera.SetActive(!Thirdcamera.activeSelf);
-            //ChangePOV();
         }
-        //else 
-        //{
-        //    MainCamera.SetActive(true);
-        //    Thirdcamera.SetActive(false);
-        //}
     }
 }
