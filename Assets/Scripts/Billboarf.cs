ef04813cc0efa80a61c657d9147cb29d2789687e
byggemandb <basmalayj@hotmail.com>
2023-11-18 21:21:02 +0100

    Merge branch 'Ny-Branch-uden-kaos' of https://github.com/Xaquzy/SpilRomerriget into Ny-Branch-uden-kaos

--- Assets/Scripts/Billboarf.cs
+++ Assets/Scripts/Billboarf.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class Billboarf : MonoBehaviour
 {
     public Transform mainCam;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         transform.LookAt(transform.position + mainCam.forward);
     }
